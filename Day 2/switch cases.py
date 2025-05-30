aeb5208bc10609395d85dd33d0bb3790fa9026c4
Aysha Nassar <ayshanassar10000@gmail.com>
2025-05-30 19:57:47 +0530

    Basics of Python-II
    .py

---
+++ Day 2/switch cases.py
@@ -0,0 +1,26 @@
+#simple switch case
+
+day=int(input("Enter a day:"))
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid Number")
+
+"""
+output:
+Enter a day:2
+Tuesday
+"""
