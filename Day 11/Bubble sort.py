6ab16851b79d0fbf11ce0517d3bb33c78e3a8bab
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-13 19:24:08 +0530

    Searching and Sorting Algorithms
    .py

--- Day 11/Bubble sort.py
+++ Day 11/Bubble sort.py
@@ -1,1 +1,15 @@
+a=list(map(int,input("Enter array elements:").split()))
+n=len(a)
+for i in range(n):
+        for j in range((i+1),n):
+            if(a[i]>a[j]):
+              t=a[i]
+              a[i]=a[j]
+              a[j]=t
+print(a)
 
+"""
+output:
+Enter array elements:5 3 2 8 1 9 0 6
+[0, 1, 2, 3, 5, 6, 8, 9]
+"""
