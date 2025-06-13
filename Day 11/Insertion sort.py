5e27ac2579bd92afa7e7786a92451646885f9115
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-13 20:11:38 +0530

    Searching and Sorting Algorithms
    .py

--- Day 11/Insertion sort.py
+++ Day 11/Insertion sort.py
@@ -1,1 +1,18 @@
+a = list(map(int, input("Enter numbers to sort: ").split()))
 
+for i in range(1, len(a)):
+    key = a[i]
+    j = i - 1
+
+    while j >= 0 and a[j] > key:
+        a[j + 1] = a[j]
+        j -= 1
+
+    a[j + 1] = key
+
+print("Sorted list:", a)
+"""
+output:
+Enter numbers to sort: 4 5 1 7 2 0 3 9
+Sorted list: [0, 1, 2, 3, 4, 5, 7, 9]
+"""
