7868d8bd69e151040b8d00b7317c4d1cb9c4eca1
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-14 15:39:06 +0530

    Python Math & Logic Problems
    .py

--- Day 12/Perfect number or not.py
+++ Day 12/Perfect number or not.py
@@ -1,1 +1,21 @@
+n = int(input("Enter a number: "))
+s = 0
 
+for i in range(1, n):
+    if n % i == 0:
+        s += i
+
+if s == n:
+    print(n, "is a Perfect Number")
+else:
+    print(n, "is not a Perfect Number")
+
+"""
+Case 1:
+Enter a number: 6
+6 is a Perfect Number
+
+Case 2:
+Enter a number: 10
+10 is not a Perfect Number
+"""
