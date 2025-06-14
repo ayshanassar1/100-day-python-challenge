e9cff1f71c2219b607223fe58f078e7270ea80c4
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-14 15:30:08 +0530

    Python Math & Logic Problems
    .py

--- Day 12/Automorphic number or not.py
+++ Day 12/Automorphic number or not.py
@@ -1,1 +1,17 @@
+n = int(input("Enter a number: "))
+s = n ** 2
 
+if str(s).endswith(str(n)):
+    print(n, "is an Automorphic number")
+else:
+    print(n, "is not an Automorphic number")
+
+"""
+Case 1:
+Enter a number: 76
+76 is an Automorphic number
+
+Case 2:
+Enter a number: 25
+25 is not an Automorphic number
+"""
