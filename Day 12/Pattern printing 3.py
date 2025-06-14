99e6d69814f35063288ef4b3752ba5ce92fbd3b3
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-14 15:37:35 +0530

    Python Math & Logic Problems
    .py

--- Day 12/Pattern printing 3.py
+++ Day 12/Pattern printing 3.py
@@ -1,1 +1,16 @@
-
+"""
+n = 3
+Output:
+  * 
+ * * 
+* * * 
+ * * 
+  * 
+"""
+n = int(input("Enter number of rows: "))
+# Upper part
+for i in range(1, n + 1):
+    print(" " * (n - i) + "* " * i)
+# Lower part
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "* " * i)
