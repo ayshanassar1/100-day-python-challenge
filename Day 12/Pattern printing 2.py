1dfb24e9c52fcec3a8f346bdc185a401419cae98
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-14 15:35:23 +0530

    Python Math & Logic Problems
    .py

--- Day 12/Pattern printing 2.py
+++ Day 12/Pattern printing 2.py
@@ -1,1 +1,13 @@
+"""
+Number of rows=3
 
+	             *
+          *    *    *
+     *       *       *     *  
+"""
+n=int(input("how much rows are required:"))
+j=1
+for i in range(1,n+1,2):
+    print(" "* (n-i) ,"* "*j)
+    j=j+2
+print("* "*j)
