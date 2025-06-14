df6ae826a21bdb1d63d462f418c9146c61e72f86
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-14 15:32:42 +0530

    Python Math & Logic Problems
    .py

--- Day 12/Harshard number or not.py
+++ Day 12/Harshard number or not.py
@@ -1,1 +1,22 @@
+n = int(input("Enter a number: "))
+s = 0
+t = n
 
+while t > 0:
+    s += t % 10
+    t //= 10
+
+if n % s == 0:
+    print(n, "is a Harshad number")
+else:
+    print(n, "is not a Harshad number")
+
+"""
+Case 1:
+Enter a number: 18
+18 is a Harshad number
+
+Case 2:
+Enter a number: 19
+19 is not a Harshad number
+"""
