60580b93ce1989df62c40614f626fd934d9dea33
Aysha Nassar <ayshanassar10000@gmail.com>
2025-06-14 15:40:32 +0530

    Python Math & Logic Problems
    .py

--- Day 12/Reverse a number.py
+++ Day 12/Reverse a number.py
@@ -1,1 +1,15 @@
+n = int(input("Enter a number: "))
+r = 0
 
+while n > 0:
+    d = n % 10
+    r = r * 10 + d
+    n = n // 10
+
+print("Reversed number is:", r)
+
+"""
+Case 1:
+Enter a number: 1234
+Reversed number is: 4321
+"""
